7aec2a95931f83f3fdf44c87de15a76cd1505312
paulprojectp <paul.projectp@gmail.com>
2025-06-04 11:54:55 -0400

    Simplify site with Express server

--- src/app/env.js
+++
@@ -1,3 +0,0 @@
-// This file sets environment variables for Next.js
-// It will be included in the client-side bundle
-process.env.NEXT_PUBLIC_BASE_PATH = process.env.NODE_ENV === 'production' ? '/ai-sports-almanac2' : '';
